42413b925fed4c61ce754583c373771bf8af4647
avishmehta <avishmehta68@gmail.com>
2023-10-17 00:33:31 +0530

    e6ugfhugo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,21 +7,29 @@ import Form2_l2 from './component/Form2_l2';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Navbar from './component/Navbar';
 import Footer from './component/Footer';
-
+import Login from './component/Login';
+import VerifyOtp from './component/VerifyOtp';
+import Project_states from './context/Project_states';
+import Alert from './component/Alert';
 function App() {
   return (
     <>
+  <Project_states>
  <BrowserRouter>
  <Navbar></Navbar>
+ <Alert/>
     <Routes>
       <Route exact path="/" element={<Home />} />
       <Route exact path="/form" element={<Form />} />
       <Route exact path="/form1_l2" element={<Form1_l2 />} />
       <Route exact path="/form2_l2" element={<Form2_l2 />}/>
+      <Route exact path="/login" element={<Login/>} />
+      <Route exact path="/verifyOtp" element={<VerifyOtp/>} />
        {/* Add the '/form' route */}
     </Routes>
   <Footer></Footer>
     </BrowserRouter>
+    </Project_states>
     </>
   );
 }
