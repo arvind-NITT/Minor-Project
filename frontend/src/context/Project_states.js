5511b46c78e518eab41dbd6f5ee91212b79b734a
avishmehta <avishmehta68@gmail.com>
2023-10-16 23:53:20 +0530

    login signup added

---
+++ frontend/src/context/Project_states.js
@@ -0,0 +1,34 @@
+import ProjectContext from "./Contexts";
+import React, { useState } from "react";
+// import { useNavigate } from "react-router-dom
+export default function Project_states(props){
+    const [username, setusername] = useState("");
+    const [alert, setAlert] = useState(null);
+    const showAlert = (message, type) => {
+        console.log("show alert called");
+        setAlert({
+          message: message,
+          type: type,
+        });
+     console.log(alert)
+        setTimeout(() => {
+          setAlert(null);
+        }, 2000);
+      };  
+      return ( 
+        <ProjectContext.Provider
+          value={{
+           
+            username,
+            setusername,
+            
+           
+            showAlert,
+            alert
+          }}
+        >
+          {props.children}
+        </ProjectContext.Provider>
+      );
+}
+ 
