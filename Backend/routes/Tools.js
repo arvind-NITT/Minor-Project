53f942855af75b4fe3991ed4679a355a17a03a83
Arvind Mali <arvindmali5768@gmail.com>
2023-11-18 23:30:33 +0530

    feteyryiuopo

--- Backend/routes/Tools.js
+++ Backend/routes/Tools.js
@@ -63,7 +63,7 @@ router.post("/submitform",AuthenticateUser,async (req, res) => {
     Role:send_to,
     Department:department, 
     FormId:result._id,
-    fileid:count,
+    fileid:fileno,
     Approved:false,
     date:Date,
    })
@@ -117,8 +117,6 @@ router.get("/FetchFormsforlevel1", AuthenticateUser, async (req, res) => {
     const Level1Forms= await Level1.find({Role:Role,Department:Department});
     // // console.log(AllForms);
     console.log(Level1Forms);
-
-
     res.send({Level1Forms});
   });
 router.post("/FetchFormsforlevel1user", AuthenticateUser, async (req, res) => {
